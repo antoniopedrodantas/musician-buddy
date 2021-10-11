1e4762d97783f9cdfb001f3e0548daafc3cc6c1c
Pedro Dantas <pedro.dantascv@gmail.com>
2021-10-11 16:18:35 +0200

    added reset button for bpm counter

--- src/components/BpmCounter.js
+++ src/components/BpmCounter.js
@@ -1,55 +1,62 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import moment from "moment";
 
 export function BpmCounter() {
+  // bpm variable
+  const [finalBpm, setFinalBpm] = useState(0);
 
-    // variables for the counter and time
-    const [BpmCounter, setBpmCounter] = useState(0);
-    const [startTime, setStartTime] = useState(0);
-    const [currentTime, setCurrentTime] = useState(moment());
+  // variables for the counter and time
+  const [BpmCounter, setBpmCounter] = useState(0);
+  const [startTime, setStartTime] = useState(0);
+  const [currentTime, setCurrentTime] = useState(moment());
 
-    // clicking mechanism
-    const handleBpmClick = () => {
+  // variable for the metronome
+  // const [isPlaying, setPlaying] = useState(false);
 
-        // bpm counter always increases
-        setBpmCounter(BpmCounter + 1);
+  // clicking mechanism
+  const handleBpmClick = () => {
+    // bpm counter always increases
+    setBpmCounter(BpmCounter + 1);
 
-        // once the user clicks the first time, the time starts counting
-        if (BpmCounter === 1){
-            setStartTime(moment().format("x"));
-        }
-
-        // sets current time and calculates the difference from the start time
-        setCurrentTime(moment().format("x"));
-        const timeDiff = currentTime - startTime;
-
-        // ignores the 0 bug
-        if(parseInt(timeDiff) !== 0) {
-
-            // calculates bpm for this tap
-            // had to put BpmCounter - 2 because of weird bug on moment library
-            // on the first 2 taps the start time isn't initialized
-            // the correct formula would be => BpmCounter / ((timeDiff / 1000) / 60)  
-            const bpm = (BpmCounter - 2) / ((timeDiff / 1000) / 60);
-
-            // prints only valid bpm times
-            if(bpm >= 0) {
-                console.log(Math.round(bpm));
-            }
-
-        }
-
-        // TODO: restarts if idle for 2 seconds
-        
+    // once the user clicks the first time, the time starts counting
+    if (BpmCounter === 1) {
+      setStartTime(moment().format("x"));
     }
 
-    return (
-        <div>
-          <button onClick={handleBpmClick}>
-              Click me
-          </button>
-        </div>
-      );
+    // sets current time and calculates the difference from the start time
+    setCurrentTime(moment().format("x"));
+    const timeDiff = currentTime - startTime;
+
+    // ignores the 0 bug
+    if (parseInt(timeDiff) !== 0) {
+      // calculates bpm for this tap
+      // had to put BpmCounter - 2 because of weird bug on moment library
+      // on the first 2 taps the start time isn't initialized
+      // the correct formula would be => BpmCounter / ((timeDiff / 1000) / 60)
+      setFinalBpm((BpmCounter - 2) / (timeDiff / 1000 / 60));
+    }
+  };
+
+  const handleResetBpm = () => {
+    setBpmCounter(0);
+    setStartTime(0);
+    setCurrentTime(moment());
+    setFinalBpm(0);
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleBpmClick}>Tap here</button>
+      </div>
+      <div>
+        <button onClick={handleResetBpm}>Reset</button>
+      </div>
+      <div>
+        <p>{Math.round(finalBpm)}</p>
+      </div>
+    </div>
+  );
 }
 
 export default BpmCounter;

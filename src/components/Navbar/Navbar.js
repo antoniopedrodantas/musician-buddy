492bc1b8f22ea989ef712474b8194ac4781e2d08
Pedro Dantas <pedro.dantascv@gmail.com>
2021-10-22 20:15:53 +0200

    started to add css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GiMusicalScore } from "react-icons/gi";
+
+// Styling
+import "./Navbar.css";
 
 export function Navbar() {
   return (
-    <nav>
+    <nav className="navbar">
       <h1>
-        <Link to="/">Musician Buddy</Link>
+        <Link className="navbar-logo" to="/">
+          <GiMusicalScore size={40} />
+        </Link>
       </h1>
-      <ul>
+      <ul className="nav-menu">
         <li>
-          <Link to="/bpm-counter">Bpm</Link>
-          <Link to="/tuner">Tuner</Link>
-          <Link to="/about">About</Link>
+          <Link className="nav-links" to="/bpm-counter">
+            Bpm
+          </Link>
+          <Link className="nav-links" to="/tuner">
+            Tuner
+          </Link>
+          <Link className="nav-links" to="/about">
+            About
+          </Link>
         </li>
       </ul>
     </nav>

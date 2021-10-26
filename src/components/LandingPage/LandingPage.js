1e39aff545a08984024cd8c7e367341323aa244c
Pedro Dantas <pedro.dantascv@gmail.com>
2021-10-26 21:01:26 +0200

    refactored looks of welcome page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { GiMetronome } from "react-icons/gi";
+import { IoMdMusicalNote } from "react-icons/io";
 
 // Styling
 import "./LandingPage.css";
@@ -13,7 +15,22 @@ export function LandingPage() {
     <div>
       <div id="home-page-container">
         <div className="home-page">
-          <div>Welcome to Musician Buddy</div>
+          <h1>Welcome to MusicianBuddy</h1>
+          <div>If you are a musician, this is the place for you</div>
+          <div className="home-page-options">
+            <a className="home-page-options-item" href="#bpm-page-container">
+              <div>
+                <GiMetronome size={120} />
+              </div>
+              <div>BPM Counter</div>
+            </a>
+            <a className="home-page-options-item" href="#tuner-page-container">
+              <div>
+                <IoMdMusicalNote size={120} />
+              </div>
+              <div>Tuner</div>
+            </a>
+          </div>
         </div>
       </div>
       <div id="bpm-page-container">
